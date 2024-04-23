0b9abdbf5d57b0fc7a12c8ab9c421dcd6fee4051
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-04-24 00:57:10 +0530

    images added

--- src/Components/Specialize.jsx
+++ src/Components/Specialize.jsx
@@ -1,5 +1,8 @@
 import styles from "./../styles/specialize.module.css";
 import img1 from "./../images/Group.png";
+import img2 from "./../images/Skincare 1.png";
+import img3 from "./../images/Immunity 1.png";
+import img4 from "./../images/Hair 1.png";
 
 const Specialize = () => {
   return (
@@ -14,19 +17,19 @@ const Specialize = () => {
         </div>
         <div className={styles.cscard}>
           <div className={styles.scard}>
-            <img src={img1} alt="" />
+            <img src={img2} alt="" />
           </div>
           <span>Skin Care</span>
         </div>
         <div className={styles.cscard}>
           <div className={styles.scard}>
-            <img src={img1} alt="" />
+            <img src={img3} alt="" />
           </div>
           <span>Immunity</span>
         </div>
         <div className={styles.cscard}>
           <div className={styles.scard}>
-            <img src={img1} alt="" />
+            <img src={img4} alt="" />
           </div>
           <span>Hair Care</span>
         </div>
